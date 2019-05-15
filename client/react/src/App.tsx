91dc26063d0cb4b3f46230a84a731e3cf452ac46
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2019-05-15 09:06:04 -0500

    Set up redux provider and app router

--- client/react/src/App.tsx
+++ client/react/src/App.tsx
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Conduit - Real World App
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import ConduitRoutes from './routes';
+
+const App: FunctionComponent<{}> = () => (
+  <>
+    <header>
+      <h1>Conduit</h1>
+    </header>
+
+    <main>
+      <ConduitRoutes />
+    </main>
+  </>
+);
 
 export default App;
