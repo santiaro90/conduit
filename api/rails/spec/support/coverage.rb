6c26a03424ec95cea3d9a6d43699faa6882209cd
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2019-05-30 18:21:32 -0500

    Don't use Codecov gem

--- api/rails/spec/support/coverage.rb
+++ api/rails/spec/support/coverage.rb
@@ -1,11 +1,8 @@
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 
 SimpleCov.start 'rails' do
   add_filter '/spec/'
-end
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
