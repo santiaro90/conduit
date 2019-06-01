37a52dd6f01db682a760bbb2223c938e993b9c94
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2019-06-01 18:24:20 -0500

    Add /signUp route

--- client/react/src/App.tsx
+++ client/react/src/App.tsx
@@ -1,12 +1,16 @@
 import React, { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 import ConduitRoutes from './routes';
+
+import { routes } from 'packages/config';
 
 const App: FunctionComponent<{}> = () => (
   <>
     <header>
       <h1>Conduit</h1>
+      <Link to={routes.signUp}>Sign Up</Link>
     </header>
 
     <main>
