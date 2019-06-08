b59ed2a1c8f0dcf58c691d97f565ea234c8fc052
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2019-06-08 12:43:23 -0500

    Improve header

--- client/react/src/App.tsx
+++ client/react/src/App.tsx
@@ -1,17 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 import ConduitRoutes from './routes';
-
-import { routes } from 'packages/config';
+import { Header } from 'packages/core/components';
 
 const App: FunctionComponent<{}> = () => (
   <>
-    <header>
-      <h1>Conduit</h1>
-      <Link to={routes.signUp}>Sign Up</Link>
-    </header>
+    <Header />
 
     <main>
       <Container>
